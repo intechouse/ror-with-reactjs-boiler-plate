b4b6a900575761caba133c402de6c707b899ae15
sajidalirordevloper <iammsajidali@gmail.com>
2021-04-16 03:02:11 +0500

    routes set

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-
-  namespace :api do
-    scope :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
-    end
-  end
-  
-  devise_for :users
+  mount_devise_token_auth_for 'User' , at: "api/v1/auth"
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
