ab0729d2ae6e2f3e729c7f038983d892cb79316b
UsmanFayyaz <usman.fayyaz.3131@gmail.com>
2021-04-21 02:25:32 +0500

    redux-token-auth setup

---
+++ app/javascript/services/Constants.js
@@ -0,0 +1,18 @@
+export const WEBSITE_BASE_URL = "http://localhost:3000";
+export const authUrl = WEBSITE_BASE_URL + "/api/v1/auth";
+
+export const userAttributes = {
+  id: "id",
+  name: "name",
+  uid: "uid",
+  username: "username",
+};
+
+export const userRegistrationAttributes = {
+  email: "email",
+  name: "name",
+  username: "username",
+  mobile: "mobile",
+  uid: "uid",
+  provider: "provider",
+};
